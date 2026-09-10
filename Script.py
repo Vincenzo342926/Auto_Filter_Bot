254c5ec7d1a755bd633f2fb13ecb54572849f5dd
Vincenzo342926 <faizubhai324926@gmail.com>
2026-09-11 02:32:00 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -268,7 +268,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/dreamxbotz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/kkranime">[ KKRANIME ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/Kdrama_knox">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/kkranime">[ KKRANIME ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
